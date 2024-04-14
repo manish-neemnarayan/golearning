de1133f16d2ab85e45866e87793cab96331b6b68
manish-neemnarayan <manish9301kumar@gmail.com>
2024-04-15 00:00:36 +0530

    channels loop setup

--- main.go
+++ main.go
@@ -5,22 +5,58 @@ import (
 	"time"
 )
 
-func main() {
-	resultch := make(chan string)
-
-	//this code is unblockable because here resultch knows that there is person-A (go anonymous func) to accept the cookie when person-B
-	//(resultch <- "fooo") gives resultch
-	//and it passs this cookie to the person-A because resultch is unbuffered means it has no box to store cookie but to handover to someone else
-	go func() {
-		result := <-resultch
-		fmt.Println(result)
-	}()
-
-	resultch <- "fooo"
-
+type Server struct {
+	quitch chan struct{} //0 bytes instead of bool which takes 1 or  2bytes
+	msgch  chan string
 }
 
-func fetchRes(n int) string {
-	time.Sleep(time.Second * 2)
-	return fmt.Sprintf(`waited for..%d`, n)
+func newServer() *Server {
+	return &Server{
+		quitch: make(chan struct{}),
+		msgch:  make(chan string, 128),
+	}
+}
+
+func (s *Server) start() {
+	fmt.Println("starting server...")
+	s.loop()
+}
+
+func (s *Server) loop() {
+mainloop:
+	for {
+		select {
+		case <-s.quitch:
+			fmt.Println("quitting server")
+			break mainloop
+		case msg := <-s.msgch:
+			s.handleMessage(msg)
+		}
+	}
+	fmt.Println("server is shutting down...")
+}
+
+func (s *Server) quit() {
+	s.quitch <- struct{}{}
+}
+
+func (s *Server) sendMessage(msg string) {
+	s.msgch <- msg
+}
+
+func (s *Server) handleMessage(msg string) {
+	fmt.Println("we received a message: ", msg)
+}
+
+func main() {
+	msgServer := newServer()
+
+	go func() {
+		msgServer.sendMessage("COULD BE THE LAST MSG ::::")
+		time.Sleep(time.Second * 5)
+		msgServer.quit()
+	}()
+
+	msgServer.start()
+
 }
