fa37f6e41cc65f492812a9d421c8584d3b8bde76
manish-neemnarayan <manish9301kumar@gmail.com>
2024-04-14 18:49:54 +0530

    channels basic

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	resultch := make(chan string)
+
+	//this code is unblockable because here resultch knows that there is person-A (go anonymous func) to accept the cookie when person-B
+	//(resultch <- "fooo") gives resultch
+	//and it passs this cookie to the person-A because resultch is unbuffered means it has no box to store cookie but to handover to someone else
+	go func() {
+		result := <-resultch
+		fmt.Println(result)
+	}()
+
+	resultch <- "fooo"
+
+}
+
+func fetchRes(n int) string {
+	time.Sleep(time.Second * 2)
+	return fmt.Sprintf(`waited for..%d`, n)
 }
